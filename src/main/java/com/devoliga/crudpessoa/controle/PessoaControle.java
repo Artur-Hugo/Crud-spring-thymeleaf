3addafbc4070ae36cc35fdcab910de8dd14e9222
Artur-Hugo <Artur.Hugoliga@gmail.com>
2022-02-07 04:42:03 -0300

    método alterar/deletar(Pessoa)

--- src/main/java/com/devoliga/crudpessoa/controle/PessoaControle.java
+++ src/main/java/com/devoliga/crudpessoa/controle/PessoaControle.java
@@ -1,10 +1,13 @@
 package com.devoliga.crudpessoa.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,4 +38,26 @@ public class PessoaControle {
 		repo.save(pessoa);
 		return "redirect:/rh/pessoas";
 	}
+	
+	@GetMapping("/rh/pessoas/{id}")
+	public String alterarPessoa(@PathVariable("id") long id, Model model) {
+		Optional<Pessoa> pessoaOpt = repo.findById(id);
+		if(pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa inválida");
+		}
+		
+		model.addAttribute("pessoa", pessoaOpt.get());
+		return "rh/pessoas/form";
+	}
+	
+@GetMapping("/rh/pessoas/excluir/{id}")
+public String excluirPessoa(@PathVariable("id") long id, Model model) {
+	Optional<Pessoa> pessoaOpt = repo.findById(id);
+	if(pessoaOpt.isEmpty()) {
+		throw new IllegalArgumentException("Pessoa inválida");
+	}
+	 repo.delete(pessoaOpt.get());
+	 
+	 return "redirect:/rh/pessoas";
+}
 }
