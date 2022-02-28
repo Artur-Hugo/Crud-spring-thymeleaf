56eae457c1c1b7f621a9324cc41b3b13f5533e08
Artur-Hugo <Artur.Hugoliga@gmail.com>
2022-02-28 12:18:59 -0300

    Entity: Endereço, funcionario. Enum: UF

--- src/main/java/com/devoliga/crudpessoa/department/entity/Cargo.java
+++ src/main/java/com/devoliga/crudpessoa/department/entity/Cargo.java
@@ -1,9 +1,12 @@
 package com.devoliga.crudpessoa.department.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Cargo extends AbstractEntity<Long> {
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
 
+	@OneToMany(mappedBy = "cargo")
+	private List<Funcionario> funcionarios;
+	
 	public String getNome() {
 		return nome;
 	}
@@ -37,6 +43,12 @@ public class Cargo extends AbstractEntity<Long> {
 		this.departamento = departamento;
 	}
 
-	
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 
 }
